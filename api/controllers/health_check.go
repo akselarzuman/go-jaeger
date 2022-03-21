b998654caa66d383d3b12892e71e6d254b079eff
aksel-arzuman <aksel.arzuman@rollicgames.com>
2022-03-22 00:59:49 +0300

    * created a new span in controller

--- api/controllers/health_check.go
+++ api/controllers/health_check.go
@@ -1,10 +1,25 @@
 package controllers
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/opentracing/opentracing-go"
+	jlog "github.com/opentracing/opentracing-go/log"
 )
 
 func HealthCheck(c *gin.Context) {
+	span, err := opentracing.StartSpanFromContext(c.Request.Context(), "health_check_controller")
+	if err != nil {
+		log.Println(err.Err())
+	}
+	defer span.Finish()
+
+	span.LogFields(
+		jlog.String("name", "Aksel"),
+		jlog.String("surname", "Arzuman"),
+	)
+
 	c.JSON(200, gin.H{
 		"message": "ok",
 	})
