b36146a2ad546b4d3e17b1684024a6a6c49f175f
aksel-arzuman <aksel.arzuman@rollicgames.com>
2022-03-25 00:15:54 +0300

    * gin opentelemetry mw

--- api/router/router.go
+++ api/router/router.go
@@ -1,14 +1,16 @@
 package router
 
 import (
+	"os"
+
 	"github.com/akselarzuman/go-jaeger/api/controllers"
-	"github.com/akselarzuman/go-jaeger/api/middlewares"
 	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 )
 
 func Setup() *gin.Engine {
 	app := gin.New()
-	app.Use(middlewares.OpentracingMiddlewareGin())
+	app.Use(otelgin.Middleware(os.Getenv("JAEGER_SERVICE_NAME")))
 	app.GET("/health_check", controllers.HealthCheck)
 
 	return app
