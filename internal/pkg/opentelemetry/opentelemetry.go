56b8a92e9245fbd505cd65b98bd741adcc1e7ea7
aksel-arzuman <aksel.arzuman@rollicgames.com>
2022-03-24 21:28:54 +0300

    * opentelemetry

---
+++ internal/pkg/opentelemetry/opentelemetry.go
@@ -0,0 +1,44 @@
+package opentelemetry
+
+import (
+	"os"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
+)
+
+// NewJaegerTraceProvider returns an OpenTelemetry TracerProvider configured to use
+// the Jaeger exporter that will send spans to the provided url. The returned
+// TracerProvider will also use a Resource configured with all the information
+// about the application.
+// Ref: https://github.com/open-telemetry/opentelemetry-go/blob/main/example/jaeger/main.go#L42
+func NewJaegerTraceProvider() (*tracesdk.TracerProvider, error) {
+	url := os.Getenv("JAEGER_URL")
+
+	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+	if err != nil {
+		return nil, err
+	}
+
+	tp := tracesdk.NewTracerProvider(
+		// Always be sure to batch in production.
+		tracesdk.WithBatcher(exp),
+		// Record information about this application in a Resource.
+		tracesdk.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(os.Getenv("JAEGER_SERVICE_NAME")),
+			attribute.String("environment", os.Getenv("ENVIRONMENT")),
+			// attribute.Int64("ID", id),
+		)),
+	)
+
+	// Register our TracerProvider as the global so any imported
+	// instrumentation in the future will default to using it.
+	otel.SetTracerProvider(tp)
+
+	return tp, nil
+}
